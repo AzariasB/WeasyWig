a847ad5bd307efd80c7ccc4ce6a4dea0feca41bc
AzariasB <>
2015-09-24 18:54:24 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Main menu</title>
+
+        <!--  Css dependencies -->
+        <link type="text/css" rel="stylesheet" href="assets/css/jquery-ui.min.css" />
+        <link type="text/css" rel="stylesheet" href="assets/css/bootstrap.min.css" />
+
+        <!-- Js dependencies -->
+        <script src="assets/js/jquery.min.js" ></script>
+        <script src="assets/js/jquery-ui.min.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+        <script src="assets/js/underscore.js"></script>
+        <script src="assets/js/backbone.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="col-xs-12 text-center">
+                <h1>WeasyWig</h1>
+            </div>
+        </div>
+    </body>
+</html>
